26aecae6dfedfd57d02813ff245fbfc568e68533
ewq3666 <145737607+ewq3666@users.noreply.github.com>
2023-10-25 16:53:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const cors = require("cors");
 const helmet = require("helmet");
 const Razorpay = require("razorpay");
 require("dotenv").config({ path: "./.env" });
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your allowed domain
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api", require("./routes/authRoute"));
 app.use("/api", require("./routes/quetionsRoute"));
+
+
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({
